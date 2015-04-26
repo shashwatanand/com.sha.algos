c1dfc9797689376873a7ac9cf55467445397e7c4
Shashwat Anand <shashwat.anand@gmail.com>
2015-04-26 19:40:39 +0530

    Quick Sort updated

--- src/com/sha/algos/sorting/QuickSort.java
+++ src/com/sha/algos/sorting/QuickSort.java
@@ -11,4 +11,33 @@ public class QuickSort {
 			System.out.println(i + " ");
 		}
 	}
+
+	private void sort(int[] input) {
+		if (input == null && input.length == 0) {
+			return;
+		}
+		this.array = input;
+		quickSort(0, this.array.length - 1);
+	}
+
+	private void quickSort(int low, int high) {
+		int i = low;
+		int j = high;
+		
+		int pivot = this.array[(low + high)/2];
+		
+		while (i <= j) {
+			while(array[i] < pivot) {
+				i++;
+			}
+			while(array[j] > pivot) {
+				j--;
+			}
+			if (i <= j) {
+				exchange(i, j);
+				i++;
+				j--;
+			}
+		}
+	}
 }
