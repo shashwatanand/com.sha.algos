fb7a2b77a4946e2792c71caed28ce821ced76b76
Shashwat Anand <shashwat.anand@gmail.com>
2015-04-26 19:40:14 +0530

    Updated Quick sort

--- src/com/sha/algos/sorting/QuickSort.java
+++ src/com/sha/algos/sorting/QuickSort.java
@@ -1,5 +1,14 @@
 package com.sha.algos.sorting;
 
 public class QuickSort {
-
+	private int[] array;
+	
+	public static void main(String[] args) {
+		QuickSort sorter = new QuickSort();
+		int[] input = { 45, 23, 11, 89, 2, 77, 98, 13, 28, 65, 43 };
+		sorter.sort(input);
+		for (int i : input) {
+			System.out.println(i + " ");
+		}
+	}
 }
