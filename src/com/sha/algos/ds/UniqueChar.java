9e61d68b091cccf5daa450449edb94af232cf630
Shashwat Anand <shashwat.anand@gmail.com>
2015-05-02 17:55:38 +0530

    updated unique character class

--- src/com/sha/algos/ds/UniqueChar.java
+++ src/com/sha/algos/ds/UniqueChar.java
@@ -10,5 +10,22 @@ public class UniqueChar {
 		String input1 = "Shashwat";
 		String input2 = "abcde";
 		System.out.println("Input string : " + input1 + " has unique characters : " + isUniqueChars(input1));
+		System.out.println("Input string : " + input2 + " has unique characters : " + isUniqueChars(input2));
+	}
+
+	private static boolean isUniqueChars(String input) {
+		if (input == null || input.length() > 128) {
+			return false;
+		}
+		
+		boolean[] set = new boolean[265];
+		for (int index = 0; index < input.length(); index++) {
+			int val = input.charAt(index);
+			if (set[val]) {
+				return false;
+			}
+			set[val] = true;
+		}
+		return true;
 	}
 }
