088c6a1c78a812536afcbbff0333b85ddf9ed40d
Shashwat Anand <shashwat.anand@gmail.com>
2015-05-02 17:48:19 +0530

    added class Unique chars

---
+++ src/com/sha/algos/ds/UniqueChar.java
@@ -0,0 +1,14 @@
+package com.sha.algos.ds;
+
+/**
+ * Implement an alogorithim to determine if string has all unique characters. What if you
+ * cannot use additional data structures
+ * @author Shashwat
+ */
+public class UniqueChar {
+	public static void main(String[] args) {
+		String input1 = "Shashwat";
+		String input2 = "abcde";
+		System.out.println("Input string : " + input1 + " has unique characters : " + isUniqueChars(input1));
+	}
+}
