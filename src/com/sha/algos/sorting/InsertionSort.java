e7fbfe6580b427cedf635454ec6d06e91e936385
Shashwat Anand <shashwat.anand@gmail.com>
2015-05-02 17:40:07 +0530

    Instertion sorf

--- src/com/sha/algos/sorting/InsertionSort.java
+++ src/com/sha/algos/sorting/InsertionSort.java
@@ -6,6 +6,19 @@ public class InsertionSort {
 		insertionSort(input);
 	}
 	
+	private static void insertionSort(int[] input) {
+		for(int j = 1; j < input.length; j++) {
+			int key = input[j];
+			int i = j - 1;
+			while((i > -1) && (input[i]) > key) {
+				input[i + 1] = input[i];
+				i--;
+			}
+			input[i + 1] = key;
+		}
+		displayArray(input);
+	}
+
 	private static void displayArray(int[] input) {
 		for (int i : input) {
 			System.out.print(i + " ");
