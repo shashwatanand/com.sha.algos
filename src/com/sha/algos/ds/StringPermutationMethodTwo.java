bab4a2f7a0886286fc347bc62d030955aefa7584
Shashwat Anand <shashwat.anand@gmail.com>
2015-05-03 19:23:02 +0530

    added impletementation

--- src/com/sha/algos/ds/StringPermutationMethodTwo.java
+++ src/com/sha/algos/ds/StringPermutationMethodTwo.java
@@ -13,4 +13,23 @@ public class StringPermutationMethodTwo {
 		System.out.println("Is " + str1 + " and " + str2 + " are permutation of "
 				+ "each other " + permutation(str1, str2));
 	}
+
+	private static boolean permutation(String str1, String str2) {
+		if (str1.length() != str2.length()) {
+			return false;
+		}
+		int[] letters = new int[256];
+		char[] arr = str1.toCharArray();
+		for (char c : arr) {
+			letters[c]++;
+		}
+		
+		for (int index = 0; index < str2.length(); index++) {
+			int c = (int)str2.charAt(index);
+			if (--letters[c] < 0) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
